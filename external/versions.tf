95f6ff2ba04f88ebd3a348bcabdf811ac86fbc5b
System administrator <root@bootstrap.localdomain>
2023-02-05 07:51:26 +0000

    Initial config

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "trantornet"
 
     workspaces {
       name = "homelab-external"
